e08f6e2f3a26711fda06678866f5766e2b2003a9
razzlepdx <rwilson.or@gmail.com>
2018-03-18 22:44:51 -0700

    add homepage test

---
+++ tests.py
@@ -0,0 +1,22 @@
+import unittest
+
+from server import app
+from model import db, connect_to_db, User, Account
+
+class UserSignUpTests(unittest.TestCase):
+    "Tests for a new user to Readerboard."
+
+    def setUp(self):
+        """setup actions for all tests in this class."""
+
+        self.client = app.test_client()
+        app.config["TESTING"] = True
+
+    def testHomePage(self):
+        """ Tests that an unauthorized user is redirected to signup page. """
+        result = self.client.get('/', follow_redirects=True)
+        self.assertIn("Enter your email here", result.data)
+        self.assertNotIn("ewraejwklarjewkla;", result.data)
+
+if  __name__ == "__main__":
+    unittest.main()
